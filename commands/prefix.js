912e97fb51b4f7b6971d5c27398797dee67fcd2f
beanc16 <bean.christopher@outlook.com>
2022-08-07 13:30:44 -0400

    Make prefix use the new serverprefixes manager

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,8 +1,8 @@
 // Library & Custom Variable
 const Command = require("./miscellaneous/command");
 const PermissionsEnum = require("../helpers/enums/PermissionsEnum");
-const ServerPrefixesManager = require("./managers/" + 
-									  "serverPrefixesManager");
+const ServerPrefixesManagerV2 = require("./managers/serverPrefixesManagerV2");
+const { logger } = require("@beanc16/logger");
 
 
 
@@ -13,29 +13,31 @@ class Prefix extends Command
 		super();
 	}
 	
-	run(bot, user, userId, channelId, message, args, currentPrefix, 
-		serverPrefixes)
+	run(bot, user, userId, channelId, message, args, currentPrefix, serverPrefixes)
     {
-		// The user CAN manage the server/guild
-		if (message.member.permissions.has("MANAGE_GUILD"))
-		{
-			// Get the new prefix based on the message (.prefix !)
-			let newPrefix = getNewPrefix(args);
+		const newPrefix = args.join(" ");
 
-			// Update the prefix if it changed
-			tryUpdatePrefix(message, currentPrefix, newPrefix);
+		// A new prefix WAS set
+		if (newPrefix !== "")
+		{
+			ServerPrefixesManagerV2.setPrefix(message, newPrefix)
+			.then(function ()
+			{
+				message.channel.send(`The prefix has been changed from \`${currentPrefix}\` to \`${newPrefix}\``);
+			})
+			.catch(function (err)
+			{
+				logger.error("Failed to set prefix", err);
+				message.channel.send(`Failed to update prefix. It will stay as \`${currentPrefix}\``);
+			});
 		}
-		
-		// The user CAN NOT manage the server/guild
+
+		// A new prefix WAS NOT set
 		else
 		{
-			// Send a message to the channel
-			message.channel.send('You do not have permission to ' + 
-								 'manage the server.');
-
-			return currentPrefix;
+			message.channel.send(`No new prefix was detected. The prefix will stay as \`${currentPrefix}\``);
 		}
-    }
+}
 	
 	getCommandAbbreviations()
 	{
@@ -60,8 +62,7 @@ class Prefix extends Command
 	
 	getHelpDescription()
 	{
-        return "Changes the prefix that must be typed before a " + 
-			   "command.";
+        return "Changes the prefix that must be typed before a command.";
 	}
 	
     getHelpExamples()
@@ -76,54 +77,3 @@ let thisCommand = new Prefix();
 
 // Export functions (for require statements in other files)
 module.exports = thisCommand.getAsJson();
-
-function getNewPrefix(args)
-{
-    // Initialize a blank prefix
-    let newPrefix = "";
-
-    // Loop over each argument
-    for (let i = 0; i < args.length; i++)
-    {
-        // If it's not the first argument, add space
-        if (i !== 0)
-        {
-            newPrefix += " ";
-        }
-
-        // Add the current argument to the new prefix
-        newPrefix += args[i];
-    }
-
-    // Return the new prefix
-    return newPrefix;
-}
-
-function tryUpdatePrefix(message, currentPrefix, newPrefix)
-{
-	// A new prefix WAS set
-	if (newPrefix !== "")
-	{
-		// Update .json file if the prefix changed
-		ServerPrefixesManager.tryUpdatePrefixJsonFile(message, 
-													  newPrefix);
-		
-		// Send a message to the channel
-		message.channel.send('The prefix has been changed from: "' +
-							 currentPrefix + '" to "' + newPrefix + 
-							 '"');
-		
-		return newPrefix;
-	}
-
-	// A new prefix WAS NOT set
-	else
-	{
-		// Send a message to the channel
-		message.channel.send('No new prefix was detected. The ' + 
-							 'prefix will stay as: ' + 
-							 '"' + currentPrefix + '"');
-
-		return currentPrefix;
-	}
-}
