d028b28d22f7b807c3061fbe7969771901d1fc21
beanc16 <bean.christopher@outlook.com>
2022-08-07 13:53:52 -0400

    Update about to not use TextHelpers

--- commands/about.js
+++ commands/about.js
@@ -1,9 +1,25 @@
 // Library & Custom Variables
 const Command = require("./miscellaneous/command");
 const PermissionsEnum = require("../helpers/enums/PermissionsEnum");
-const TextHelpers = require('../helpers/textHelpers');
+const { Text } = require("@beanc16/discordjs-helpers");
 const AboutInfo = require('./customization/aboutInfo');
 const Package = require('../package');
+
+
+
+const aboutMessageText =
+`${Text.bold(AboutInfo.botName)}
+
+${Text.italic("Version:")}
+${Package.version}
+
+${Text.italic("Created By:")}
+${Package.author}
+
+${Text.italic("Description:")}
+Hi, my name is ${AboutInfo.botName}. ${_getCreationPurpose()}
+
+${_getHomeServerInvite()}`;
 
 
 
@@ -16,29 +32,7 @@ class About extends Command
 	
 	run(bot, user, userId, channelId, message, args, prefix)
     {
-		let creationPurpose = getCreationPurpose();
-		let serverInvite = getHomeServerInvite();
-		
-        // Initialize the message to display
-        let aboutMessage = TextHelpers.getBoldText(AboutInfo.botName) +
-						   "\n\n" +
-
-                           TextHelpers.getItalicText("Version:") + "\n" +
-                           Package.version + "\n\n" +
-
-                           TextHelpers.getItalicText("Created by:") +
-						   "\n" +
-                           Package.author + "\n\n" +
-
-                           TextHelpers.getItalicText("Description:") +
-						   "\n" +
-                           "Hi, my name is " + AboutInfo.botName + 
-						   ". " + creationPurpose + "\n\n" +
-
-                           serverInvite;
-
-        // Send a message to the channel
-        message.channel.send(aboutMessage);
+        message.channel.send(aboutMessageText);
     }
 	
 	getCommandAbbreviations()
@@ -83,37 +77,19 @@ module.exports = thisCommand.getAsJson();
 
 
 
-function getCreationPurpose()
+function _getCreationPurpose()
 {
-	let creationPurpose;
-	if (AboutInfo.creationPurpose != null)
-	{
-		creationPurpose = AboutInfo.creationPurpose;
-	}
-	else
-	{
-		creationPurpose = "";
-	}
-	
-	return creationPurpose;
+	return AboutInfo.creationPurpose || "";
 }
 
-function getHomeServerInvite()
+function _getHomeServerInvite()
 {
-	let serverInvite;
-	if (AboutInfo.homeServerInvite != null)
-	{
-		serverInvite = TextHelpers.getItalicText("Feedback and " +
-											 "More Info:") + "\n" +
-					   "To give feedback or to learn more about me " + 
-					   "and my creator, join my home discord " + 
-					   "server here:\n" +
-					   AboutInfo.homeServerInvite;
-	}
-	else
-	{
-		serverInvite = "";
-	}
-	
-	return serverInvite;
+	return (AboutInfo.homeServerInvite)
+
+		? `${Text.italic("Feedback and More Info:")}
+			To give feedback or to learn more about me and my creator, join my home discord server here:
+			${AboutInfo.homeServerInvite}`
+			.split("\t").join("")	// Remove tabs.
+
+		: "";
 }
