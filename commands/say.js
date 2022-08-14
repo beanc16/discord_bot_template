8d1b39d1f737b5c8d57cb043e9b6c65e12596bfc
beanc16 <bean.christopher@outlook.com>
2022-08-14 13:57:05 -0400

    Make say use BaseCommand

--- commands/say.js
+++ commands/say.js
@@ -1,69 +1,72 @@
 // Library & Custom Variable
-const Command = require("./miscellaneous/command");
+const BaseCommand = require("./miscellaneous/BaseCommand");
 const { permissionsEnum } = require("@beanc16/discordjs-helpers");
 
-class Say extends Command
-{
-	constructor()
-	{
-		super();
-	}
-	
-	run(bot, user, userId, channelId, message, args, prefix)
-	{
-        // Form a message from what the user said after the command
-		const msg = args.join(" ");
 
+
+class Say extends BaseCommand
+{
+    async run({
+        args,
+        attachments,
+        bot,
+        channel,
+        helpers: {
+            allBotCommandNames,
+            allBotCommandAbbreviations,
+            botHasCommand,
+            botGetCommand,
+        },
+        prefix,
+        message,
+        reactions,
+        server,
+        user,
+    })
+	{
 		// The user DID include a message
-		if (msg.length > 0)
+		if (args.length > 0)
 		{
-			// Send a message to the channel
+			const msg = args.join(" ");
 			message.channel.send(msg);
 		}
 
 		// The user DID NOT include a message
 		else
 		{
-			// Send a message to the channel
 			message.channel.send("No message to say detected.");
 		}
 	}
-	
-	getCommandAbbreviations()
+
+
+
+    get abbreviations()
+    {
+        return [
+            `${this.commandName}`,
+			"repeat",
+        ];
+    }
+
+
+
+    /*
+     * Help documentation
+     */
+
+    get description()
+    {
+        return `Make me repeat what you say.`;
+    }
+
+	get examples()
 	{
-		return super.getCommandAbbreviations("say", "repeat");
-	}
-	
-	getRequiredPermissions()
-	{
-		return [];
-	}
-	
-	
-	
-	/**********************
-	 * HELP DOCUMENTATION *
-	 **********************/
-	
-	getCommandName()
-	{
-		return super.getCommandName(__filename);
-	}
-	
-	getHelpDescription()
-	{
-		return "Make me repeat what you say.";
-	}
-	
-	getHelpExamples()
-	{
-		return super.getHelpExamples("say Hi!");
+		return [
+			`${this.commandName} Hi!`,
+		];
 	}
 }
 
 
 
-let thisCommand = new Say();
-
-// Export functions (for require statements in other files)
-module.exports = thisCommand.getAsJson();
+module.exports = new Say();
